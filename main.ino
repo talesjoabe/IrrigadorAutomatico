428253ad6aba9158c64eb59f1a50155bb8125314
talesjoabe <30672549+talesjoabe@users.noreply.github.com>
2019-10-21 19:20:46 -0300

    Adição da interrupção

--- main.ino
+++ main.ino
@@ -16,6 +16,9 @@
 #define turnLedOn(PORT, bits) PORT&=~(1<<bits)
 #define turnLedOff(PORT,bits) PORT|=(1<<bits)
 #define tst_bit(PIN,bits)(PIN&(1<<bits)) //testa o bit x da variável Y (retorna 0 ou 1)
+
+int cont = 0;
+bool state = false;
 //------------------------------------------------------------------------------------
 
 // configuração do ADC
@@ -31,12 +34,42 @@ void set_ADC(void)
 
 long mapFunction(long adc, long in_min, long in_max, long out_min, int out_max)
 { 
-  /*
-  if(in_min<100){
-    in_min=100;
-  }*/
   return (adc - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+void set_counter()
+{
+  /*
+   * Configura-se o contador
+   * 1. COM0A1 - PWM NÃO INVERTIDO;
+   * 2. WGM01 e WGM00 - MODO DE OPERAÇÃO DE 10 BITS;
+   * 3. APÓS SETAR WGM12, PWM OPERA NO MODO FAST; 
+   * 4. CS02 - HABILITA-SE UM DIVISOR DE 256 (DEFINE A TAXA DE SÍMBOLO DE TRANSMISSÃO);
+   * 5. ativa a interrupção por estouro do TIMER 0.
+   */
+  cli();
+  TCNT0 =  0b00000000;
+  //TCCR0A = _BV(COM0A1) | _BV(WGM01) | _BV(WGM00);
+  TCCR0B = _BV(WGM02) | _BV(CS02);
+  TIMSK0 = _BV(TOIE0);
+  sei();
+}
+
+ISR(TIMER0_OVF_vect){
+  cont++;
+  if(cont == 1225){
+    state = !state;
+    cont = 0;
+  }
+
+  // -- Configura Interrupção do Timer0 --
+  //
+  // T0_OVF = (256 - timer0) x prescaler x ciclo de máquina
+  //        = (256 -    0  ) x    256    x      62,5E-9
+  //        =~ 4 ms
+  //
+  // Para 60 ms: 4ms x 15
+} 
 
 void setPwmOutput(){
    // iniciamos zerando o valor do output do pwm de TCCR1A
@@ -51,12 +84,9 @@ void setPwmOutput(){
    * 3. CS11 - HABILITA-SE UM DIVISOR DE 8 (DEFINE A TAXA DE SÍMBOLO DE TRANSMISSÃO);
    * 4. APÓS SETAR WGM12, PWM OPERA NO MODO FAST;
    */
-  TCCR1A = _BV(COM1A1) 
-           | _BV(WGM10);
-  TCCR1B = _BV(CS11) 
-           | _BV(WGM12);
+  TCCR1A = _BV(COM1A1) | _BV(WGM10);
+  TCCR1B = _BV(CS11) | _BV(WGM12);
 
-  
   DDRB = _BV(PB1);
 }
 
@@ -66,10 +96,15 @@ void potenciometroEnable(uint8_t ad_value){
        while (!(ADCSRA & 0b00010000)); // aguarda conversao
        ad_value = mapFunction(ADC, 0, 1023, 0, 255);
        Serial.println(ad_value);
-       OCR1A = ad_value;
+      
+       if(state) {
+        OCR1A = ad_value; 
+       } else OCR1A=0;
+       
        if(botaoPressionado(PIND, botao)){
          // sei();
           OCR1A= 0;
+          state = !state;
           break;
        }
      }
@@ -87,21 +122,24 @@ int main()
   * Configura todas as portas como 
   */
  PORTD= 0b11111111; 
- int cont=0;
+ bool system_on = false;
+
+ set_counter();
  
   while(1){    
-    if(cont==0){
+    if(!system_on){
       turnLedOn(PORTD,LED2);
-      cont++; 
+      system_on = true; 
     }
     
      if(botaoPressionado(PIND, botao))
      {
        while(botaoPressionado(PIND, botao)); // enquanto eu não soltar o botão
-         if(tst_bit(PORTD,LED1)){ 
+       cont = 0;
+       if(tst_bit(PORTD,LED1)){ 
          turnLedOn(PORTD, LED1);
          turnLedOff(PORTD,LED2);
-
+          
          setPwmOutput();
          potenciometroEnable(ad_value);
        }
